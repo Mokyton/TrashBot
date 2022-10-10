214476904859928a94eb7941d4507269c8c3368a
Chillwave Strong <chillwav@si-b3.kzn.21-school.ru>
2022-10-10 11:28:02 +0300

    test

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,11 +1,65 @@
 package telegram
 
-import "github.com/Mokyton/TrashBot/clients/telegram"
+import (
+	"github.com/Mokyton/TrashBot/clients/telegram"
+	"github.com/Mokyton/TrashBot/events"
+	"github.com/Mokyton/TrashBot/lib/e"
+	"github.com/Mokyton/TrashBot/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(client *te)
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      nil,
+		storage: nil,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	update, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+	res := make([]events.Event, 0, len(update))
+
+	for _, u := range update {
+		res = append(res, event(u))
+	}
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+	if updType == events.Message {
+		res.Meta{
+
+		}
+	}
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+	return events.Message
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+}
